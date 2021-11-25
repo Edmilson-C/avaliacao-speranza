fbb01a9a14d4c093a4359f07b405c05644c5c6b7
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 19:12:30 +0200

    style: remove height & bg texture and add bottom padding in app.css

--- src/components/form-preview/form-preview.jsx
+++ src/components/form-preview/form-preview.jsx
@@ -17,8 +17,8 @@ const FormPreview = ({
   return (
     <Card
       sx={{
-        backgroundColor: '#E2C275',
-        color: '#FBFBFB',
+        backgroundColor: 'var(--color-white)',
+        color: 'var(--color-black)',
         borderRadius: '1.2rem',
         padding: '1rem'
       }}
@@ -30,7 +30,7 @@ const FormPreview = ({
         <Typography sx={{ fontSize: '1.6rem', marginTop: '1rem' }}>{detail}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" sx={{ fontSize: '1.2rem', padding: '1rem' }} onClick={changeForm}>
+        <Button size="small" sx={{ fontSize: '1.2rem', padding: '1rem', color: 'var(--color-black)' }} onClick={changeForm}>
           Ver formulário
         </Button>
       </CardActions>
