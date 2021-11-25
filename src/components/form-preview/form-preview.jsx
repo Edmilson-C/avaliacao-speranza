145c16764adf8887b0e26eb13457f9b8a917b8ad
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 02:08:32 +0200

    refactor: remove placeholder text and put form name & description in form preview

--- src/components/form-preview/form-preview.jsx
+++ src/components/form-preview/form-preview.jsx
@@ -3,17 +3,20 @@ import {
   Button, Card, CardContent, CardActions, Typography
 } from '@mui/material'
 
-const FormPreview = () => (
+const FormPreview = ({ title, detail, ...otherProps }) => (
   <Card
     sx={{
-      backgroundColor: '#E2C275', color: '#FBFBFB', borderRadius: '1.2rem', padding: '1rem'
+      backgroundColor: '#E2C275',
+      color: '#FBFBFB',
+      borderRadius: '1.2rem',
+      padding: '1rem'
     }}
   >
     <CardContent>
       <Typography variant="h3" component="div">
-        Form name
+        {title}
       </Typography>
-      <Typography sx={{ fontSize: '1.6rem', marginTop: '1rem' }}>Form Description</Typography>
+      <Typography sx={{ fontSize: '1.6rem', marginTop: '1rem' }}>{detail}</Typography>
     </CardContent>
     <CardActions>
       <Button size="small" sx={{ fontSize: '1.2rem', padding: '1rem' }}>
