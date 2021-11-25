bab9b533192e76ce5e54b89377fb4ab5cfa5fa2a
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 19:33:54 +0200

    add new box container in form overview

--- src/components/form-overview/form-overview.jsx
+++ src/components/form-overview/form-overview.jsx
@@ -11,22 +11,23 @@ const FormOverview = () => {
   const { currentForm, form } = useContext(FormsContext)
 
   return (
-    <Box
-      margin="3.2rem auto"
-      display="grid"
-      gridTemplateColumns="repeat(auto-fit, 40rem)"
-      gap={2}
-      justifyContent="center"
-    >
-      {form.map(({ formName, description, ...otherProps }, index) => (
-        <FormPreview
-          key={index}
-          title={formName}
-          detail={description}
-          position={index}
-          {...otherProps}
-        />
-      ))}
+    <Box margin="3.2rem auto">
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fit, minmax(20rem, 1fr))"
+        gap={2}
+        justifyContent="center"
+      >
+        {form.map(({ formName, description, ...otherProps }, index) => (
+          <FormPreview
+            key={index}
+            title={formName}
+            detail={description}
+            position={index}
+            {...otherProps}
+          />
+        ))}
+      </Box>
 
       {currentForm > -1 && <Form />}
     </Box>
