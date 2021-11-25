b48c36b97e5e14102c738e05c50e2827d7849b5a
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 17:05:41 +0200

    get form data from Forms Context

--- src/components/form-preview/form-preview.jsx
+++ src/components/form-preview/form-preview.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
   Button, Card, CardContent, CardActions, Typography
 } from '@mui/material'
 
-const FormPreview = ({ title, detail, ...otherProps }) => (
-  <Card
-    sx={{
-      backgroundColor: '#E2C275',
-      color: '#FBFBFB',
-      borderRadius: '1.2rem',
-      padding: '1rem'
-    }}
-  >
-    <CardContent>
-      <Typography variant="h3" component="div">
-        {title}
-      </Typography>
-      <Typography sx={{ fontSize: '1.6rem', marginTop: '1rem' }}>{detail}</Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small" sx={{ fontSize: '1.2rem', padding: '1rem' }}>
-        Ver formulário
-      </Button>
-    </CardActions>
-  </Card>
-)
+import { FormsContext } from '../../context/forms/forms.context'
+
+const FormPreview = ({
+  title, detail, position, ...otherProps
+}) => {
+  const { showForm } = useContext(FormsContext)
+
+  const changeForm = () => {
+    showForm(position)
+  }
+
+  return (
+    <Card
+      sx={{
+        backgroundColor: '#E2C275',
+        color: '#FBFBFB',
+        borderRadius: '1.2rem',
+        padding: '1rem'
+      }}
+    >
+      <CardContent>
+        <Typography variant="h3" component="div">
+          {title}
+        </Typography>
+        <Typography sx={{ fontSize: '1.6rem', marginTop: '1rem' }}>{detail}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" sx={{ fontSize: '1.2rem', padding: '1rem' }} onClick={changeForm}>
+          Ver formulário
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
 
 export default FormPreview
