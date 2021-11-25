59f508d07e5fdd997afe99b17bb708e3cbe67bd0
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 02:23:07 +0200

    add form overview to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/header/header.component'
+import FormOverview from './components/form-overview/form-overview'
 
 import './App.css'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <FormOverview />
     </div>
   )
 }
