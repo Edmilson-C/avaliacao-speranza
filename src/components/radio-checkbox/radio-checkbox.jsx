4bb837828f72f19c8749a836aacbb66aba89c92a
edmilson-c <edmilson.conceicao09@gmail.com>
2021-11-25 04:35:01 +0200

    feat: create Form control label component

---
+++ src/components/radio-checkbox/radio-checkbox.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FormControlLabel } from '@mui/material'
+
+const RadioCheckbox = ({ ...props }) => (
+  <FormControlLabel {...props} />
+)
+
+export default RadioCheckbox // Find better name
